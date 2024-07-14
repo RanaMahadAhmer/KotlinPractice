d0cfca3a7f9cdb48aa8437fac0b525f74fbccb26
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-14 14:53:51 +0500

    Added Icon

--- app/src/main/java/com/ranamahadahmer/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ranamahadahmer/unitconverter/MainActivity.kt
@@ -2,26 +2,26 @@ package com.ranamahadahmer.unitconverter
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
-
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
 
 
-                        )
+                    )
 
 
                 }
@@ -52,24 +52,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting( ) {
-    Column ( verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
-
+fun Greeting() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(
             text = "Unit Converter!",
             fontWeight = FontWeight(1000)
 
         )
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = { println() }, placeholder = { Text("Enter a value") })
+        OutlinedTextField(
+            value = "",
+            onValueChange = { x -> println(x) },
+            placeholder = { Text("Enter a value") })
         Spacer(modifier = Modifier.height(16.dp))
         Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text("Convert")
+            Box {
+                Button(onClick = { }) {
+                    Text("Convert")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Convert")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Meter") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Inches") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                }
             }
-            Spacer(modifier = Modifier.width(40.dp))
-            Button(onClick = { /*TODO*/ }) {
-                Text("Convert")
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = {}) {
+                    Text("Convert")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Convert")
+                }
+
+                DropdownMenu(expanded = true, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Meter") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Inches") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                }
             }
         }
     }
