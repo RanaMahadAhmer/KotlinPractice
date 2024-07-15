8c14f0c738bf46870a95f30f7ad10b75c4516ea7
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-15 22:23:21 +0500

    improved

--- app/src/main/java/com/ranamahadahmer/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ranamahadahmer/unitconverter/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -23,9 +24,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ranamahadahmer.unitconverter.ui.theme.UnitConverterTheme
@@ -43,21 +49,45 @@ class MainActivity : ComponentActivity() {
 
 
                     )
-
-
                 }
             }
         }
     }
 }
 
+val conversion = mapOf(
+    "Meter-Foot" to 3.28084,
+    "Meter-Inches" to 36,
+    "Meter-Centimetre" to 100,
+    "Foot-Meter" to 0.3048,
+    "Foot-Inches" to 12,
+    "Foot-Centimetre" to 30.480,
+    "Inches-Meter" to 0.02540,
+    "Inches-Foot" to 0.08333,
+    "Inches-Centimetre" to 2.5399,
+    "Centimetre-Meter" to 0.01,
+    "Centimetre-Foot" to 0.03280,
+    "Centimetre-Inches" to 0.3937008,
+)
+val units = listOf<String>("Meter", "Foot", "Inches", "Centimetre")
+
+
 @Composable
 fun Greeting() {
+    var inputUnit by remember { mutableStateOf(false) }
+    var outputUnit by remember { mutableStateOf(false) }
+    var inputUnitSelected by remember { mutableStateOf(units.first()) }
+    var outputUnitSelected by remember { mutableStateOf(units.first()) }
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
+
         Text(
             text = "Unit Converter!",
             fontWeight = FontWeight(1000)
@@ -65,38 +95,61 @@ fun Greeting() {
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = { x -> println(x) },
+            value = input,
+            onValueChange = { value ->
+                input = value.replace(",", "").replace("(", "").replace(")", "").replace(",", "")
+                    .replace("+", "").replace("-", "").replace("N", "").replace("/", "")
+                    .replace("#", "").replace(" ", "").replace(";", "").replace("*", "")
+
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             placeholder = { Text("Enter a value") })
         Spacer(modifier = Modifier.height(16.dp))
         Row {
             Box {
-                Button(onClick = { }) {
+
+                Button(onClick = { inputUnit = !inputUnit }) {
                     Text("Convert")
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Convert")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {}) {
-                    DropdownMenuItem(text = { Text("Meter") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Inches") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                DropdownMenu(expanded = inputUnit, onDismissRequest = { inputUnit = false }) {
+                    for (unit in units) {
+                        DropdownMenuItem(text = { Text(unit) }, onClick = {
+                            inputUnitSelected = unit
+                            inputUnit = false
+                        })
+                    }
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box {
-                Button(onClick = {}) {
+                Button(onClick = { outputUnit = !outputUnit }) {
                     Text("Convert")
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Convert")
                 }
 
-                DropdownMenu(expanded = true, onDismissRequest = {}) {
-                    DropdownMenuItem(text = { Text("Meter") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Inches") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                DropdownMenu(expanded = outputUnit, onDismissRequest = { outputUnit = false }) {
+                    for (unit in units) {
+                        DropdownMenuItem(text = { Text(unit) }, onClick = {
+                            outputUnitSelected = unit
+                            outputUnit = false
+                            output =
+                                (input.toFloat() * conversion["$inputUnitSelected-$outputUnitSelected"].toString()
+                                    .toFloat()).toString()
+
+                        })
+                    }
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "The result is: $output",
+            fontWeight = FontWeight(600)
+
+        )
+
     }
 }
 
